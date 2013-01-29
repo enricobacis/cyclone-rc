6dbd86ce2826053faa4ac9b770288131243c6df7
enricobacis <enrico.bacis@gmail.com>
2013-01-29 18:57:46 +0100

    Version 1.4 Check file size and extension before submitting

--- js/ajax.form.js
+++ js/ajax.form.js
@@ -2,7 +2,15 @@ $(document).ready(function(){
 
   var form = document.getElementById('compilerform');
 
-  var makeRequest = function(event){
+  function showResult(output) {
+    $("#output").html(output);
+    $("#output").slideDown(400, function() {
+      $("#compiling").hide();
+      $("#submit").show();
+    });
+  }
+
+  var makeRequest = function(event) {
     // this is the REAL cross-browser way to prevent submitting!
     event.preventDefault();
 
@@ -11,26 +19,22 @@ $(document).ready(function(){
         // progress = document.querySelector('progress'),
         // textprogress = document.querySelector('i'),
 
-        updateProgress = function(evt){
+        updateProgress = function(evt) {
           // var howmuch = (evt.loaded / evt.total) * 100;
           // textprogress.innerHTML = ''+ Math.ceil(howmuch);
           // progress.value = howmuch;
         },
 
-        updateLoading = function(evt){
+        updateLoading = function(evt) {
           // progress.value = 100;
         },
 
-        onLoadHandler = function(evt){
-          $("#output").html(xhr.responseText);
-          $("#output").slideDown(400, function() {
-            $("#compiling").hide();
-            $("#submit").show();
-          });
-          $(document).ready(function(){
-            $("#expanderHead").click(function(){
+        onLoadHandler = function(evt) {
+          showResult(xhr.responseText);
+          $(document).ready(function() {
+            $("#expanderHead").click(function() {
               $("#expanderContent").slideToggle();
-              if ($("#expanderSign").text() === "show"){
+              if ($("#expanderSign").text() === "show") {
                 $("#expanderSign").html("hide");
               }
               else {
@@ -52,10 +56,20 @@ $(document).ready(function(){
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
+    var file = document.getElementById('file').files[0];
+
     $("#compiling").show();
     $("#submit").hide();
     $("#output").slideUp(200, function() {
-      makeRequest(event);
+        if (!file) {
+          showResult("<p class=\"error\">No file selected</p>");
+        } else if (file.name.split('.').pop() !== "cyc") {
+          showResult("<p class=\"error\">File must have extension <i>.cyc</i></p>");
+        } else if(file.size >= (200 * 1024)) {
+          showResult("<p class=\"error\">File must be smaller than 200 kB</p>");
+        } else {
+          makeRequest(event);
+        }
     });
   });
 
